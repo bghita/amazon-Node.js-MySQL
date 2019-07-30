e6becfaebcb72156d817abe6d2e79aedcfaac277
Becky Ghita <ghita109@gmail.com>
2019-07-30 12:27:44 -0700

    working customer database

--- customer.js
+++ customer.js
@@ -1,5 +1,4 @@
 var mysql = require("mysql");
-
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
@@ -18,3 +17,41 @@ connection.connect(function (err, res) {
     startazon();
 });
 
+function startazon() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.table(res, ["item_id", "product_name", "price"]);
+        inquirer.prompt([
+            {
+                type: "number",
+                message: "Which product by Item ID# would you like to purchase?",
+                name: "itemSelect"
+            },
+            {
+                type: "number",
+                message: `How many would you like to buy?`,
+                name: "desiredQuantity"
+            }
+        ]).then(function (answers) {
+            // get item_id 's stock_quantity value
+            var chosenItem = res.filter(each => each.item_id === answers.itemSelect)
+            // console.table(chosenItem);
+            if (answers.desiredQuantity <= chosenItem[0].stock_quantity) {
+                // console.log(`Inventory available for purchase.`)
+                var newQuantity = chosenItem[0].stock_quantity - answers.desiredQuantity;
+                var newTotalCost = answers.desiredQuantity * chosenItem[0].price
+                connection.query(
+                    "UPDATE products SET ? WHERE ?", [{ stock_quantity: newQuantity }, { item_id: answers.itemSelect }],
+                    function (err, result) {
+                        if (err) throw err;
+                        console.log(`Order Processed. You purchased ${answers.desiredQuantity} ${chosenItem[0].product_name} and were charged $${newTotalCost.toFixed(2)}`);
+                        connection.end();
+                    }
+                )
+            } else {
+                console.log(`Insufficient quantity!`)
+                connection.end();
+            }
+        })
+    });
+}
